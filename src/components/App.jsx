b902c4dbac6d718c955e1dbec0cb84fcc95b3ca5
LeiderIP <leiderpaez13@gmail.com>
2024-03-30 11:23:25 -0500

    Second task completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Profile from 'components/Profile'; // Importa el componente Profile
+import Statistics from 'components/Statistics'; // Importa el componente Statistics
 import userData from 'json/user.json'; // Importa los datos del usuario
+import statsData from 'json/data.json'; // Importa los datos de estadísticas
 
 const App = () => {
   return (
     <div>
-      {/* Renderiza el componente Profile y pasa los datos del usuario como props */}
+      {/* Tarea 1: Renderiza el componente Profile y pasa los datos del usuario como props */}
       <Profile
         username={userData.username}
         tag={userData.tag}
@@ -14,6 +16,9 @@ const App = () => {
         avatar={userData.avatar}
         stats={userData.stats}
       />
+
+      {/* Tarea 2: Renderiza el componente Statistics y pasa los datos de estadísticas como props */}
+      <Statistics title="Upload stats" stats={statsData} />
     </div>
   );
 };
@@ -23,6 +28,5 @@ createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
-
 
 export default App;
