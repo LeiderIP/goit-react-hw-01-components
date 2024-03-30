6f2755e13ac7bf19ba3624a6da77b587771cdd80
LeiderIP <leiderpaez13@gmail.com>
2024-03-30 12:06:42 -0500

    third task done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Profile from 'components/Profile'; // Importa el componente Profile
-import Statistics from 'components/Statistics'; // Importa el componente Statistics
-import userData from 'json/user.json'; // Importa los datos del usuario
-import statsData from 'json/data.json'; // Importa los datos de estadísticas
+import Profile from 'components/Profile'; 
+import Statistics from 'components/Statistics'; 
+import FriendList from 'components/FriendList'; 
+import userData from 'json/user.json'; 
+import statsData from 'json/data.json'; 
+import friendsData from 'json/friends.json'; 
 
 const App = () => {
   return (
     <div>
-      {/* Tarea 1: Renderiza el componente Profile y pasa los datos del usuario como props */}
       <Profile
         username={userData.username}
         tag={userData.tag}
@@ -16,9 +17,8 @@ const App = () => {
         avatar={userData.avatar}
         stats={userData.stats}
       />
-
-      {/* Tarea 2: Renderiza el componente Statistics y pasa los datos de estadísticas como props */}
       <Statistics title="Upload stats" stats={statsData} />
+      <FriendList friends={friendsData} />
     </div>
   );
 };
